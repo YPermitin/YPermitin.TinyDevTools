1c13378a68673c25e5e081fe09b18390cc77df48
Permitin Yury <ypermitin@yandex.ru>
2022-09-13 20:27:55 +0500

    Реализовал экспорт в Excel
    - Добавил выгрузку коллекции сформированных GUID'ов в Excel. Экспорт работает полностью на клиенте
    - Оптимизировал формирование коллекции GUID'ов, убрал хранение лишних данных.

---
+++ YPermitin.TinyDevTools.Client/Extensions/JSRuntime/JSRuntimeExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+
+namespace YPermitin.TinyDevTools.Client.Extensions.JSRuntime
+{
+    public static class JSRuntimeExtension
+    {
+        public static async Task SaveAs(this IJSRuntime jsRuntime, string fileName, byte[] data)
+        {
+            await jsRuntime.InvokeAsync<object>(
+                "saveAsFile",
+                fileName,
+                Convert.ToBase64String(data));
+        }
+    }
+}
